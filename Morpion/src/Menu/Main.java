3cf34c3888c8313494015f8d7c9394c7865a44ef
Theo <Theo@Theo-PC>
2018-08-19 00:35:48 +0200

    Ajouts de ActionListener + Réorganistaion des class dans Menu

--- Morpion/src/Menu/Main.java
+++ Morpion/src/Menu/Main.java
@@ -1,48 +1,26 @@
 package Menu;
 
 
-import java.awt.BorderLayout;
-import java.awt.Font;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
-	
-	private static JLabel title = new JLabel("Jeu du morpion");
-	
-	private static JButton IA = new JButton("Joueur VS IA");
-	private static JButton player = new JButton("Joueur VS Joueur");
-	
-	private static JPanel PANtitle = new JPanel();
-	private static JPanel PANbutton = new JPanel();
-	
+
 	public static void main(String[] args) {
 		
-		JFrame frame = new JFrame();
+		Frame frame = new Frame();
 		
-		frame.setTitle("Morpion");
-		frame.setSize(600, 500);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		frame.setLayout(new BorderLayout());
-		
-		
-		title.setFont(new Font("Arial", Font.BOLD, 20));
-		PANtitle.add(title);
-		
-		PANbutton.add(IA);
-		PANbutton.add(player);
-		
-		frame.add(PANtitle, BorderLayout.NORTH);
-		frame.add(PANbutton, BorderLayout.CENTER);
-		
-		
-		frame.setVisible(true);
 		
 	}
+	
+	
+	
+	
+	public class Listener implements ActionListener {
 
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated constructor stub
+		}
+
+	}
 }
