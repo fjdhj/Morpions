a7babc2e493e50986e95fedeb90923204c437b82
Theo <Theo@Theo-PC>
2018-08-18 15:11:29 +0200

    Suite de la class main

--- Morpion/src/Menu/Main.java
+++ Morpion/src/Menu/Main.java
@@ -1,10 +1,31 @@
 package Menu;
 
+
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		JFrame frame = new JFrame();
+		
+		frame.setTitle("Morpion");
+		frame.setSize(600, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setLayout(null);
+		
+		JLabel title = new JLabel("Jeu du morpion");
+		title.setFont(new Font("Arial", Font.BOLD, 20));
 
+		
+		
+		frame.setVisible(true);
+		
 	}
 
 }
