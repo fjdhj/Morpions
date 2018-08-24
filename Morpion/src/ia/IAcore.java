3fd94cf5d448b8bc4208fce6b5b7791041b6ea1f
Theo <Theo@Theo-PC>
2018-08-24 20:00:02 +0200

    Ajout du stockage des pion dans IA

--- Morpion/src/ia/IAcore.java
+++ Morpion/src/ia/IAcore.java
@@ -1,5 +1,6 @@
 package ia;
 
+import java.awt.Color;
 import java.util.ArrayList;
 
 import gamelogic.GameLogic;
@@ -12,6 +13,7 @@ public class IAcore extends Thread{
 	private ArrayList<Jeton> JetonList;
 	private int PLAYER_ID = InputsManagerIA.PLAYER_ID;
 	private int IdTurn;
+	private int gameState[][];
 	
 	public IAcore(InputsManagerIA IAinputs, GameLogic gamemode) {
 		this.IAinputs = IAinputs;
@@ -19,6 +21,11 @@ public class IAcore extends Thread{
 		IdTurn = gamemode.getIdTurn();
 	}
 
+	/*
+	 * Joueur : rond ROUGE (joueur n°2)
+	 * IA : croix BLEU (joueur n°1)
+	 * 
+	 */
 
 
 	@Override
@@ -31,9 +38,37 @@ public class IAcore extends Thread{
 				e.printStackTrace();
 			}
 			if(IdTurn != PLAYER_ID) {
+				
+				int gameState[][] = { {0, 0, 0}, {0, 0, 0}, {0, 0, 0} };
+				
+				for(Jeton jeton:JetonList) {
+					int x = jeton.getX();
+					int y = jeton.getY();
+					Color color = jeton.getCouleur();
+					
+					if(color.getRed() == 255) { //Si pion joueur
+						gameState[x-1][y-1] = 2;
+						
+					}else { //Si pion IA
+						gameState[x-1][y-1] = 1;
+						
+					}
+					
+					
+					
+				}
+				
+				for(int y = 0; y < 3; y++) {
+					for(int i = 0; i < 3; i++) {
+						
+					}
+				}
+				
 				IAinputs.pressACase(1, 1);
 				IAinputs.pressACase(2, 1);
 				IAinputs.pressACase(3, 1);
+				
+				
 			}
 		}		
 	}
